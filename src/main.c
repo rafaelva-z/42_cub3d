51ac379a2707a505551d00fcf09694ee535fc0d3
rafaelva-z <rafaelvaz.art@gmail.com>
2024-01-26 00:10:31 +0000

    merge fix

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fda-estr <fda-estr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rvaz <rvaz@student.42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 20:34:35 by rvaz              #+#    #+#             */
-/*   Updated: 2024/01/25 21:46:18 by rvaz             ###   ########.fr       */
+/*   Updated: 2024/01/26 00:10:10 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	main(int argc, char **argv)
 		return (1);			// Error handling
 	initializer(&data);
 	parser(&data, argv[1]);
-	minimap(&data);
-	// raycast(&data);	
+	//minimap(&data);
+	// raycast(&data);
 	mlx_key_hook(data.mlx_win, key_reader, &data);
 	mlx_hook(data.mlx_win, 17, 0L, close_pgm, &data);
 	mlx_loop(data.mlx);
