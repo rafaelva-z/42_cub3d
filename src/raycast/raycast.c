5867e2a9cedb58ee7e7153378528ba5925ecacdd
rafaelva-z <rafaelvaz.art@gmail.com>
2024-02-03 21:46:03 +0000

    adding textures WIP

--- src/raycast/raycast.c
+++ src/raycast/raycast.c
@@ -6,27 +6,43 @@
 /*   By: rvaz <rvaz@student.42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:41:12 by rvaz              #+#    #+#             */
-/*   Updated: 2024/02/03 16:19:44 by rvaz             ###   ########.fr       */
+/*   Updated: 2024/02/03 21:45:23 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-static void	draw_column(t_data *data, int r, int wall_height)
+static void	draw_column(t_data *data, int r, t_ray *ray)
 {
-	int	sky_size;
+	int		sky_size;
+	t_img	*texture;
 
-	sky_size = ((WIN_HEIGHT - wall_height) / 2) + data->player.vertical;
-	if (sky_size + wall_height > WIN_HEIGHT)
-		sky_size = WIN_HEIGHT - wall_height;
-	else if (sky_size <= 0)
-		sky_size = 1;
+	if (ray->side == 0)
+	{
+		if (data->player.pos.x > ray->last_hit.x)
+			texture = &data->image.east_img;
+		else
+			texture = &data->image.west_img;
+	}
+	else
+	{
+		if (data->player.pos.y > ray->last_hit.y)
+			texture = &data->image.south_img;
+		else
+			texture = &data->image.north_img;
+	}
+	sky_size = ((WIN_HEIGHT - ray->wall_height) / 2) + data->player.vertical;
+	if (sky_size + ray->wall_height > WIN_HEIGHT)
+		sky_size = WIN_HEIGHT - ray->wall_height;
+	// else if (sky_size <= 0)
+	// 	sky_size = 1;
 	draw_vertical_line((t_2d_point){r, 0},
 		sky_size, data->img, SKY_COLOR);
-	draw_vertical_line((t_2d_point){r, sky_size + wall_height},
-		WIN_HEIGHT - (sky_size + wall_height), data->img, FLOOR_COLOR);
-	draw_vertical_line((t_2d_point){r, sky_size},
-		wall_height, data->img, 0);
+	draw_vertical_line((t_2d_point){r, sky_size + ray->wall_height},
+		WIN_HEIGHT - (sky_size + ray->wall_height), data->img, FLOOR_COLOR);
+	// draw_vertical_line((t_2d_point){r, sky_size},
+	// 	ray->wall_height, data->img, 0);
+	draw_vertical_line_texture((t_2d_point){r, sky_size}, texture, data, ray);
 }
 
 /**
@@ -49,7 +65,6 @@ void	raycast(t_data *data)
 	int			r;
 	t_ray		ray;
 	double		camera_x;
-	int			wall_height;
 
 	r = 0;
 	while (r < WIN_WIDTH)
@@ -57,8 +72,9 @@ void	raycast(t_data *data)
 		camera_x = 2 * r / (double)WIN_WIDTH - 1;
 		ray.dir.x = data->player.dir.x + data->player.plane.x * camera_x;
 		ray.dir.y = data->player.dir.y + data->player.plane.y * camera_x;
-		wall_height = get_wall_height(dda(&ray, data) * (data->player.fov * 0.0151));
-		draw_column(data, r, wall_height);
+		dda(&ray, data);
+		ray.wall_height = get_wall_height(ray.distance * (data->player.fov * 0.0151));
+		draw_column(data, r, &ray);
 		r++;
 	}
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img->img, 0, 0);
--- src/utils/draw_line.c
+++ src/utils/draw_line.c
@@ -6,11 +6,38 @@
 /*   By: rvaz <rvaz@student.42lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 17:45:18 by rvaz              #+#    #+#             */
-/*   Updated: 2024/02/03 02:13:29 by rvaz             ###   ########.fr       */
+/*   Updated: 2024/02/03 20:23:27 by rvaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+
+void	draw_vertical_line_texture(t_2d_point start, t_img *texture, t_data *data, t_ray *ray)
+{
+	int			y_pos;
+	t_2d_point	t_pos;
+	double		t_step;
+	int			size;
+
+	size = ray->wall_height;
+	t_step = (double)TEXTURE_HEIGHT / (double)ray->wall_height;
+	y_pos = start.y;
+	int t_x_start = (ray->last_hit.x - (int)ray->last_hit.x) * TEXTURE_WIDTH;
+	t_pos = (t_2d_point){t_x_start, 0};
+
+	if (start.x >= WIN_WIDTH || start.y >= WIN_HEIGHT || size <= 0)
+		return ;
+	while (size > 0)
+	{
+		if (start.x >= 0 && y_pos >= 0
+			&& y_pos < WIN_HEIGHT && start.x < WIN_WIDTH)
+			draw_pixel(data->img, start.x, y_pos, texture->color_grid[(int)t_pos.x][(int)t_pos.y]);
+		y_pos++;
+		size--;
+		t_pos.y += t_step;
+	}
+	//printf("last hit: %f %f | t_pos_x: %f | t_pos_y: %f\n", ray->last_hit.x, ray->last_hit.y, t_pos.x, t_pos.y);
+}
 
 void	draw_vertical_line(t_2d_point start, int size, t_img *img, int color)
 {
